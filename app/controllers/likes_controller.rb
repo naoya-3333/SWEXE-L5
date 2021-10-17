4ecfc8fafb79057845aac9d5b2fd3d59c748e420
Ubuntu <ubuntu@ip-172-31-54-177.ec2.internal>
2021-10-17 12:16:05 +0000

    いいねを押せる回数を１回までに

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,9 +1,14 @@
 class LikesController < ApplicationController
   def create
+    
     tweet = Tweet.find(params[:tweet_id])
     user = User.find_by(uid: session[:login_uid])
-    user.like_tweets << tweet
-    redirect_to root_path
+    if Like.find_by(user_id: user.id)
+      redirect_to root_path
+    else
+      user.like_tweets << tweet
+      redirect_to root_path
+    end
   end
   
   def destroy
